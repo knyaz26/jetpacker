8c2cc4c83d7faba169f1c2e155de05e5bb151194
knyaz <saba2006.diasamidze@gmail.com>
2025-03-18 13:59:46 +0400

    added enemy ability to aim

--- game/enemy/enemy.gd
+++ game/enemy/enemy.gd
@@ -5,6 +5,7 @@ func _ready():
 	
 func _process(delta: float):
 	face_player()
+	gun_aim()
 	
 func face_player():
 	if GameManager.position_player.x > global_position.x:
@@ -12,9 +13,15 @@ func face_player():
 		$CPUParticles2D.position.x = -5
 		$AnimatedSprite2D2.flip_h = false
 		$AnimatedSprite2D2.offset.x = 2
+		$AnimatedSprite2D2/firepoint.position.x = 8
 	else:
 		$AnimatedSprite2D.flip_h = true
 		$CPUParticles2D.position.x = 5
 		$AnimatedSprite2D2.flip_h = true
 		$AnimatedSprite2D2.offset.x = -2
+		$AnimatedSprite2D2/firepoint.position.x = -8
 	
+func gun_aim():
+	$AnimatedSprite2D2.look_at(GameManager.position_player)
+	if !GameManager.position_player.x > global_position.x:
+		$AnimatedSprite2D2.rotation_degrees += 180
