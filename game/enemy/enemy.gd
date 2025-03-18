5404f482adf14a62d9c1982f5063c238178aa092
knyaz <saba2006.diasamidze@gmail.com>
2025-03-18 13:32:35 +0400

    made all enemy attributes face player

--- game/enemy/enemy.gd
+++ game/enemy/enemy.gd
@@ -9,6 +9,12 @@ func _process(delta: float):
 func face_player():
 	if GameManager.position_player.x > global_position.x:
 		$AnimatedSprite2D.flip_h = false
+		$CPUParticles2D.position.x = -5
+		$AnimatedSprite2D2.flip_h = false
+		$AnimatedSprite2D2.offset.x = 2
 	else:
 		$AnimatedSprite2D.flip_h = true
+		$CPUParticles2D.position.x = 5
+		$AnimatedSprite2D2.flip_h = true
+		$AnimatedSprite2D2.offset.x = -2
 	
