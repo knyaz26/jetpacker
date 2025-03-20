e6b4c00e31d2d81471ff1cfa277d575666a6de15
knyaz <saba2006.diasamidze@gmail.com>
2025-03-20 14:33:52 +0400

    added enemy spawn logic

--- game_manager.gd
+++ game_manager.gd
@@ -2,9 +2,20 @@ extends Node
 
 @onready var position_player = Vector2(0, 0)
 @onready var score = 0
+@onready var timer: Timer
+
+@onready var enemy_scene = preload("res://game/enemy/enemy.tscn")
 
 func _ready() -> void:
-	pass
+	timer = Timer.new()
+	add_child(timer)
+	timer.start(7)
+	timer.timeout.connect(func(): 
+		var enemy_instance = enemy_scene.instantiate()
+		get_tree().root.add_child(enemy_instance)
+		var enemy_spawn_position = Vector2(400 * cos(randi_range(0, 360)), 400 * sin(randi_range(0, 360)))
+		enemy_instance.global_position = enemy_spawn_position
+		)
 
 
 func _process(delta: float) -> void:
